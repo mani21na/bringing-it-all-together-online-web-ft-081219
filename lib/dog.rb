1bdd56ac4e5ab035443c30066762a2ff9fdaa2e7
Hanna Lee <mani21na@gmail.com>
2019-09-28 05:35:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,7 +87,12 @@ class Dog
   
   def self.find_by_name(name)
     sql = <<-SQL
-      
+      SELECT * 
+      FROM dogs 
+      WHERE name = ? 
+      LIMIT 1
     SQL
+    
+    new_dog = DB[:conn].execute(sql,name)[0]
   end
 end
